384d6f25cb6fd293ab2a9eed1510e9330d49fdf7
krulik <iwillkissyou@gmail.com>
2022-06-17 08:05:44 +0300

    phase 2 grades

---
+++ skm-dom/average-nc-mar-22/phase-2/grades.js
@@ -0,0 +1,11 @@
+export const grades = [
+  {name: 'Leonardo', grade: 45},
+  {name: 'Donatello', grade: 62},
+  {name: 'Rafael', grade: 89},
+  {name: 'Michelangelo', grade: 91},
+  {name: 'Splinter', grade: 66},
+  {name: 'Shredder', grade: 77},
+  {name: 'Roadsteady', grade: 90},
+  {name: 'Bebop', grade: 32},
+  {name: 'Krang', grade: 100}
+];
---
+++ skm-dom/average-nc-mar-22/phase-2/index.js
@@ -0,0 +1,38 @@
+import {grades} from './grades.js';
+
+function getNumbers() {
+  const numTds = document.querySelectorAll('tbody td:nth-child(2)');
+  const numbers = [];
+  for (let i = 0; i < numTds.length; i++) {
+    numbers.push(parseInt(numTds[i].textContent));
+  }
+  return numbers;
+}
+
+function getAverage(numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum / numbers.length;
+}
+
+function printAverage(average) {
+  const outputCell = document.querySelector('tfoot td:nth-child(2)');
+  outputCell.textContent = average;
+}
+
+function handleEvent(e) {
+  printAverage(
+    getAverage(
+      getNumbers()
+    )
+  );
+}
+
+function buildTable(grades) {
+
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', handleEvent);
