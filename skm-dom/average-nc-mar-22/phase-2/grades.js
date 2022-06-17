31c59d16e5f4a82c0430a04457ab6a514ffe66c2
krulik <iwillkissyou@gmail.com>
2022-06-17 13:46:27 +0300

    phase 2 grades

--- skm-dom/average-nc-mar-22/phase-2/grades.js
+++ skm-dom/average-nc-mar-22/phase-2/grades.js
@@ -1,11 +1,12 @@
 export const grades = [
-  {name: 'Leonardo', grade: 45},
-  {name: 'Donatello', grade: 62},
-  {name: 'Rafael', grade: 89},
-  {name: 'Michelangelo', grade: 91},
-  {name: 'Splinter', grade: 66},
-  {name: 'Shredder', grade: 77},
-  {name: 'Roadsteady', grade: 90},
-  {name: 'Bebop', grade: 32},
-  {name: 'Krang', grade: 100}
+  {name: 'Leonardo', grade: 82, color: 'blue', type: 'turtle'},
+  {name: 'Donatello', grade: 100, color: 'purple', type: 'turtle'},
+  {name: 'Rafael', grade: 45, color: 'red', type: 'turtle'},
+  {name: 'Michelangelo', grade: 53, color: 'orange', type: 'turtle'},
+  {name: 'Splinter', grade: 100, color: 'gray', type: 'rat'},
+  {name: 'Shredder', grade: 67, color: 'purple', type: 'human'},
+  {name: 'Rocksteady', grade: 14, color: 'lightgray', type: 'rhino'},
+  {name: 'Bebop', grade: 24, color: 'brown', type: 'boar'},
+  {name: 'Krang', grade: 100, color: 'pink', type: 'brain'},
+  {name: 'April', grade: 100, color: 'gold', type: 'human'}
 ];
--- skm-dom/average-nc-mar-22/phase-2/index.js
+++ skm-dom/average-nc-mar-22/phase-2/index.js
@@ -19,7 +19,7 @@ function getAverage(numbers) {
 
 function printAverage(average) {
   const outputCell = document.querySelector('tfoot td:nth-child(2)');
-  outputCell.textContent = average;
+  outputCell.textContent = average.toFixed(1);
 }
 
 function handleEvent(e) {
@@ -30,9 +30,70 @@ function handleEvent(e) {
   );
 }
 
-function buildTable(grades) {
-
+function getRows(grades) {
+  return grades.map(student => {
+    const row = document.createElement('tr');
+    const name = document.createElement('td');
+    const grade = document.createElement('td');
+    name.append(student.name);
+    grade.append(student.grade);
+    row.append(name, grade);
+    return row;
+  });
 }
+
+function getColumn(key, value) {
+  const column = document.createElement('td');
+  switch (key) {
+      case 'name':
+        column.style.fontFamily = 'cursive';
+        column.append(value);
+        break;
+      case 'grade':
+        column.style.fontFamily = 'monospace';
+        column.append(value);
+        break;
+      case 'color':
+        column.style.backgroundColor = value;
+        column.style.width = '30px';
+        break;
+      default:
+        column.append(value);
+        break;
+  }
+  return column;
+}
+
+function getRowsDynamic(grades) {
+  return grades.map(student => {
+    const row = document.createElement('tr');
+    for (let [key, value] of Object.entries(student)) {
+      row.append(getColumn(key, value));
+    }
+    return row;
+  });
+}
+
+function getColumns(grades) {
+  return Object.keys(grades[0]).map(name => {
+    const td = document.createElement('th');
+    td.append(name);
+    return td;
+  });
+}
+
+function buildTable(columns, rows) {
+  const header = document.querySelector('thead > tr');
+  header.append(...columns)
+
+  const tbody = document.querySelector('tbody');
+  tbody.append(...rows);
+}
+
+buildTable(
+  getColumns(grades),
+  getRowsDynamic(grades)
+);
 
 const button = document.querySelector('button');
 button.addEventListener('click', handleEvent);
