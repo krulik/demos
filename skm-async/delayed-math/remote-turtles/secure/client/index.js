477f66f11aa481191cde7e9348a96c8ce66bf4be
krulik <iwillkissyou@gmail.com>
2023-03-17 16:05:55 +0200

    secure solution

---
+++ skm-async/delayed-math/remote-turtles/secure/client/index.js
@@ -0,0 +1,90 @@
+function sendJson(method, path, body) {
+  const BASE_URL = 'https://localhost:3000';
+  return fetch(`${BASE_URL}${path}`, {
+    method,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include',
+    body: JSON.stringify(body)
+  }).then((response) => {
+    if (response.status > 204) {
+      throw response.statusText;
+    }
+    return response.json();
+  });
+}
+
+function me() {
+  return sendJson('GET', '/player/me');
+}
+
+function makeMove({inputNumber, functionName}) {
+  return sendJson('PUT', `/player/${functionName}`, {
+    inputNumber
+  });
+}
+
+function choosePlayer(playerId) {
+  return sendJson('POST', `/player/choose`, {
+    playerId
+  });
+}
+
+function setState(newState) {
+  state = {...state, ...newState};
+  console.log(state);
+}
+
+function info(message) {
+  document.querySelector('output').innerHTML = `
+    <span style="color: green;">${message}</span>
+  `;
+}
+
+function error(message) {
+  document.querySelector('output').innerHTML = `
+    <span style="color: red;">${message}</span>
+  `;
+}
+
+let state = {
+  playerId: undefined,
+  inputNumber: undefined,
+  functionName: undefined
+};
+
+// Retrieve playerId from server on first load
+me().then(({playerId}) => {
+  setState({playerId});
+  document.forms['players'].player.value = playerId;
+}).catch((err) => {
+  if (err !== 'Unauthorized') {
+    error(err);
+  }
+});
+
+document.forms['players'].addEventListener('input', (e) => {
+  choosePlayer(e.target.value)
+    .then(({message}) => {
+      info(message);
+    })
+    .catch(error);
+});
+
+document.forms['move'].addEventListener('input', (e) => {
+  if (e.target.name === 'method') {
+    setState({functionName: e.target.value});
+  } else if (e.target.type === 'number') {
+    setState({inputNumber: Number(e.target.value)});
+  }
+});
+
+document.querySelector('button').addEventListener('click', (e) => {
+  e.preventDefault();
+  makeMove(state)
+    .then(({result, message}) => {
+      info(message);
+    })
+    .catch(error);
+});
---
+++ skm-async/delayed-math/remote-turtles/secure/server/index.js
@@ -0,0 +1,126 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import fs from 'fs';
+import https from 'https';
+import cookieSession from 'cookie-session';
+
+const MINUTE = 1 * 60 * 1000;
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors({
+  origin: 'https://localhost:5500',
+  methods: ['GET', 'POST', 'PUT'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true
+}));
+
+app.use(cookieSession({
+  name: 'playerId',
+  secret: 'krang',
+  sameSite: 'none',
+  secure: true,
+  httpOnly: true,
+  maxAge: 1 * MINUTE
+}));
+
+let options = {
+  key: fs.readFileSync('./localhost-key.pem'),
+  cert: fs.readFileSync('./localhost.pem')
+};
+
+let server = https.createServer(options, app);
+server.listen(3000, () => {
+  console.log(`Server listening on port=3000`);
+});
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  if (a < b) {
+    return `Result must be positive...`;
+  }
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    return `Can't divide by zero...`;
+  }
+  return a / b;
+}
+
+let mathFunctions = {
+  'add': add,
+  'multiply': multiply,
+  'divide': divide,
+  'subtract': subtract
+};
+
+let db = {
+  'player1': {lastResult: 0},
+  'player2': {lastResult: 0}
+};
+
+function authMiddleware(request, response, next) {
+  let playerId = request.session.playerId;
+  if (!playerId || !(playerId in db)) {
+    return response.sendStatus(401);
+  }
+  return next();
+}
+
+function refreshMiddleware(request, response, next) {
+  request.session.refreshTime = Date.now();
+  return next();
+}
+
+app.get('/player/me', authMiddleware, refreshMiddleware, (request, response) => {
+  response.json({
+    playerId: request.session.playerId
+  });
+});
+
+app.put('/player/:functionName', authMiddleware, refreshMiddleware, (request, response) => {
+  let playerId = request.session.playerId;
+
+  if (!(request.params.functionName in mathFunctions)) {
+    return response.sendStatus(400);
+  }
+  let {inputNumber} = request.body;
+  if (isNaN(inputNumber)) {
+    return response.sendStatus(400);
+  }
+
+  let fn = mathFunctions[request.params.functionName];
+
+  let {lastResult} = db[playerId];
+  let result = fn(lastResult, inputNumber);
+
+  db[playerId].lastResult = result;
+
+  response.json({
+    result,
+    message: `Result for ${playerId} is ${result}`
+  });
+});
+
+app.post('/player/choose', (request, response) => {
+  let {playerId} = request.body;
+  if (!playerId || !(playerId in db)) {
+    return response.sendStatus(401);
+  }
+  request.session.playerId = playerId;
+  response.json({
+    message: `You've chosen player [${playerId}]`
+  });
+});
